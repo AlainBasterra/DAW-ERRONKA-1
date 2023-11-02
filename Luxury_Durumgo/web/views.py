8d6db88bd5b773ac85e6068e7c9f21324f0b1ccb
AlainBasterra <basterra.alain@uni.eus>
2023-11-02 10:36:59 +0100

    contact sortu

--- Luxury_Durumgo/web/views.py
+++ Luxury_Durumgo/web/views.py
@@ -8,6 +8,9 @@ def index(request):
 def login(request):
     return render(request, 'login.html')
 
+def contact(request):
+    return render(request, 'contact.html')
+
 def login_egin(request):
     post_helbidea = request.POST.get('helbidea')
     post_helbidea = request.POST.get('pasahitza')
