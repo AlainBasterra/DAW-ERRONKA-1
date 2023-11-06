899c10027e415347f36b82b004d35bedc94f1a1d
Markel Rodriguez <96288766+MarkelRodriguez@users.noreply.github.com>
2023-11-06 09:07:51 +0100

    login proba

--- Luxury_Durumgo/web/static/js/loginscripts.js
+++ Luxury_Durumgo/web/static/js/loginscripts.js
@@ -2,18 +2,18 @@ $(document).ready(function () {
     $('#login').on('submit', function (e) {
         var url = "login_egin/";
         e.preventDefault();
-        //let csrftoken = '{{ csrf_token }}'
+        
         $.ajax({
             type: 'POST',
-            //headers:{'X-CSRFToken':csrftoken},
+            
             url:url,
             data: $(this).serialize(),
             success: function (data) {
                 if (data.success){
                     alert("logina ondo dago");
-                    usuarioLogeado = true;
+                    
                     window.location.href = data.redirect_url;
-                    mostrarusuario(usuarioLogeado);
+                    
                 }
                 else{
                     alert("errorea egon da " + data.message)
